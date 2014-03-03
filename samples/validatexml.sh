58e099bd1f866636becdd908d7f81ab2c8d50af5
James Gould <jgould@verisign.com>
2014-03-03 16:28:02 -0500

    Added xmlvalidator-1.0, updated validatexml.sh to use xmlvalidator-1.0, added the xmlschemas.txt file to load the XSD files in the correct order, removed the allocationToken-1.0.xsd from the samples directory, and removed the schemaLocation references within the sample XML files.

--- samples/validatexml.sh
+++ samples/validatexml.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-XERCESJ_LIB=./lib/xerces
-export CLASSPATH=$XERCESJ_LIB/xercesImpl.jar:$XERCESJ_LIB/xercesSamples.jar:$XERCESJ_LIB/xml-apis.jar
-date
-java dom.Counter -n -v -s -f $@ 
-date
+../xmlvalidator-1.0/xmlvalidator -schemasDir . -schemasFile xmlschemas.txt $@
